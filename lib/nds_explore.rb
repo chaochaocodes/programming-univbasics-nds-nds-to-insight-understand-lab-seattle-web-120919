01f1d721eb3b46d2e143a7375c120c4b3c0e991a
honorable-exception-1170 <aliciachao@gmail.com>
2019-12-02 02:37:02 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,12 +10,14 @@ end
 
 pretty_print_nds(directors_database)
 
-
 def print_first_directors_movie_titles
-director_index = 0
-while director_index < directors_database[0].length do
-  title_string = directors_database[director_index][:movie_titles]
-  puts "#{title_string}\n"
+  director_index = 0
+  while director_index < directors_database.length do
+  element_index = 0
+    while element_index < directors_database[director_index].length do
+    title_string = directors_database[director_index][element_index]
+    puts "#{title_string}\n" 
+    element_index += 1
+    end
   director_index += 1
- end
-end
+  end
